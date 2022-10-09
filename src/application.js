8e64c787d42b116b70db8ea7be83efea98c35076
Artem Perminov <perminov.artem@eurostavka.ru>
2022-10-09 17:04:35 +0300

    add step3

--- src/application.js
+++ src/application.js
@@ -4,21 +4,43 @@ import axios from 'axios';
 import watcher from './view.js';
 import elements from './page-elements.js';
 
+const validate = (url, feeds) => {
+  const schema = yup.string().required().url().notOneOf(feeds);
+  return schema.validate(url, { abortEarly: false });
+};
+
 const getProxiedUrl = (url) => {
-  const proxyUrl = new URL('/get', 'https://allorigins.hexlet.app');
-  proxyUrl.search = new URLSearchParams({ disableCache: true, url });
+  const proxy = 'https://allorigins.hexlet.app';
+  const params = { disableCache: true, url };
+  const proxyUrl = new URL('/get', proxy);
+  proxyUrl.search = new URLSearchParams(params);
   return proxyUrl.toString();
 };
 
 export default () => {
+  const defaultLanguage = 'ru';
   const i18n = i18next.createInstance();
+
   i18n
     .init({
-      lng: `ru`,
+      lng: defaultLanguage,
+      debug: true,
       resources: {
-        errors: {
-          urlError: 'Ссылка должна быть валидным URL',
-          alreadyExist: 'RSS уже существует',
+        ru: {
+          translation: {
+            errors: {
+              urlError: 'Ссылка должна быть валидным URL',
+              alreadyExist: 'RSS уже существует',
+              rssError: 'Ресурс не содержит валидный RSS',
+              networkError: 'Ошибка сети, попробуйте позже',
+              somethingWrong: 'Что-то пошло не так',
+            },
+            loading: 'Идет загрузка',
+            success: 'RSS успешно загружен',
+            posts: 'Посты',
+            feeds: 'Фиды',
+            inspect: 'Просмотр',
+          },
         },
       },
     })
@@ -39,30 +61,38 @@ export default () => {
       errors: '',
     },
     links: [],
+    feeds: [],
+    posts: [],
+    currentPosts: {},
+    alreadyReadPosts: [],
   };
+
+  const watchedState = watcher(elements, i18n, state);
 
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
+    watchedState.form.process = 'loading';
+    watchedState.form.errors = null;
+
     const form = new FormData(e.target);
     const url = form.get('url');
-    const schema = yup.string().required().url().notOneOf(watcher(elements, i18n, state).links);
-    schema
-      .validate(url)
+
+    validate(url, watchedState.links)
       .then((validUrl) => {
         axios
           .get(getProxiedUrl(validUrl))
           .then(() => {
-            watcher(elements, i18n, state).links.push(validUrl);
-            watcher(elements, i18n, state).form.process = 'success';
+            watchedState.links.push(validUrl);
+            watchedState.form.process = 'success';
           })
           .catch((err) => {
-            watcher(elements, i18n, state).form.process = 'failed';
-            watcher(elements, i18n, state).form.errors = err.name;
+            watchedState.form.process = 'failed';
+            watchedState.form.errors = err.name;
           });
       })
       .catch((err) => {
-        watcher(elements, i18n, state).form.process = 'failed';
-        watcher(elements, i18n, state).form.errors = err.errors.join();
+        watchedState.form.process = 'failed';
+        watchedState.form.errors = err.errors.join();
       });
   });
 };
--- src/view.js
+++ src/view.js
@@ -1,41 +1,71 @@
 import onChange from 'on-change';
 
+const renderErrorss = (elements, i18n, value) => {
+  if (!value) {
+    return;
+  }
+  const { feedback } = elements;
+
+  switch (value) {
+    case 'errors.urlError':
+      feedback.textContent = i18n.t(value);
+      break;
+
+    case 'errors.alreadyExist':
+      feedback.textContent = i18n.t(value);
+      break;
+
+    default:
+      break;
+  }
+};
+
+const handleProcessSubmit = (elements) => {
+  const { form, input, button } = elements;
+  form.reset();
+  input.focus();
+  button.disabled = false;
+};
+
+const renderStatus = (elements, i18n, value) => {
+  const { input, feedback, button } = elements;
+
+  switch (value) {
+    case null:
+      break;
+    case 'success':
+      input.classList.remove('is-invalid');
+      feedback.classList.remove('text-danger');
+      feedback.classList.add('text-success');
+      feedback.textContent = i18n.t(value);
+      break;
+    case 'failed':
+      input.classList.add('is-invalid');
+      feedback.classList.remove('text-success');
+      feedback.classList.remove('text-secondary');
+      feedback.classList.add('text-danger');
+      button.disabled = false;
+      break;
+    default:
+      break;
+  }
+};
+
 export default (elements, i18n, state) =>
   onChange(state, (path, value) => {
-    const { input, feedback, button, form } = elements;
     switch (path) {
       case 'form.process':
-        switch (value) {
-          case 'success':
-            input.classList.remove('is-invalid');
-            feedback.classList.replace('text-secondary', 'text-success');
-            break;
-          case 'failed':
-            input.classList.add('is-invalid');
-            feedback.classList.remove('text-success');
-            feedback.classList.remove('text-secondary');
-            feedback.classList.add('text-danger');
-            button.disabled = false;
-            break;
-          default:
-            break;
-        }
+        renderStatus(elements, i18n, value);
         break;
+
       case 'links':
-        form.reset();
-        input.focus();
-        button.disabled = false;
+        handleProcessSubmit(elements);
         break;
+
       case 'form.errors':
-        switch (value) {
-          case 'errors.urlError':
-            break;
-          case 'errors.alreadyExist':
-            break;
-          default:
-            break;
-        }
+        renderErrorss(elements, i18n, value);
         break;
+
       default:
         break;
     }
